244a4f3f98ae1d060436cca43573c1c1e7240b56
SuzyWu2014 <shujin.wu.osu@gmail.com>
2017-02-18 23:27:26 -0800

    init playbook

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-ansible-roles
+
+## Set up Local Ubuntu Environment
+`ansible-playbook ubuntu-env.yml --ask-sudo-pass`
+
--- ubuntu-ansible.sh
+++ ubuntu-ansible.sh
@@ -1,3 +1,5 @@
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-ansible-on-ubuntu-14-04
+
 sudo apt update
 sudo apt install software-properties-common 
 sudo apt-add-repository ppa:ansible/ansible
